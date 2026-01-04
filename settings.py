5b9320305f0d836774d5c99a37523b1d07405d69
Moritzvona <moritz@alvensleben.org>
2026-01-04 20:03:46 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,27 +1,31 @@
 from os import environ
-SESSION_CONFIGS = [dict(name='investment_experiment', display_name="Investment Portfolio Study", app_sequence=['investment_experiment'], num_demo_participants=100)]
-SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1.00, participation_fee=0.00, doc="")
+
+SESSION_CONFIGS = [
+    dict(
+        name='investment_experiment',
+        display_name="Investment Portfolio Study",
+        app_sequence=['investment_experiment'],
+        num_demo_participants=100,
+    ),
+]
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00,
+    participation_fee=0.00,
+    doc=""
+)
+
 PARTICIPANT_FIELDS = []
 SESSION_FIELDS = []
+
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'EUR'
 USE_POINTS = False
+
 ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD', 'admin')
+
 DEMO_PAGE_INTRO_HTML = ""
 SECRET_KEY = '1234567890'
+
 INSTALLED_APPS = ['otree']
-```
-
----
-
-**Datei 10: `Procfile`** (im Hauptverzeichnis)
-```
-web: otree prodserver --port=$PORT
-```
-
----
-
-**Datei 11: `requirements.txt`** (im Hauptverzeichnis)
-```
-otree>=5.0.0
